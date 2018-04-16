9c119dba31d5c5577c7300db1bf0a5bed6c18b44
Krystian Szabat <221662@edu.p.lodz.pl>
2018-04-16 15:05:45 +0200

    [MC-74][MC-75] - Change message format, add scroll to body

--- chatbot_frontend/chatbot/src/app/components/chatview/chatview.component.ts
+++ chatbot_frontend/chatbot/src/app/components/chatview/chatview.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { DataService } from '../../services/data.service';
 import { Message } from '../../models/message';
 
@@ -8,6 +8,8 @@ import { Message } from '../../models/message';
   styleUrls: ['./chatview.component.scss']
 })
 export class ChatviewComponent implements OnInit {
+
+  @ViewChild('chatBody') el: ElementRef;
 
   isSending: boolean;
   message: string;
@@ -22,13 +24,25 @@ export class ChatviewComponent implements OnInit {
   }
   sendMessage() {
     if (this.message) {
-      const tab = ['Hello', 'What category do you like?', 'Older or newer film? ', 
-      'Which actor do you like?', 'Matrix, Matrix 2', 'Just google bro. I am tired', 'What did you say?'];
-      console.log(this.message);
+
+      const tab = ['Hello', 'What category do you like?', 'Older or newer film? ',
+        'Which actor do you like?', 'Matrix, Matrix 2', 'Just google bro. I am tired', 'What did you say?'];
+
       this.isSending = true;
       this.messages.push(new Message(`dsa`, this.message, true));
-      setTimeout(() => { this.isSending = false; this.message = '';
+      this.message = '';
+
+      setTimeout(() => {
+        this.el.nativeElement.scrollTo(0, this.el.nativeElement.scrollHeight);
+      }, 10);
+
+      setTimeout(() => {
+        this.isSending = false;
         this.messages.push(new Message(`dsa`, tab[this.x], false));
+
+        setTimeout(() => {
+          this.el.nativeElement.scrollTo(0, this.el.nativeElement.scrollHeight);
+        }, 10);
       }, 2000);
       this.x++;
     }
--- chatbot_frontend/chatbot/src/app/services/data.service.ts
+++ chatbot_frontend/chatbot/src/app/services/data.service.ts
@@ -1,15 +1,13 @@
 import { Injectable } from '@angular/core';
 import { Message } from '../models/message';
-import {Observable} from 'rxjs';
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class DataService {
 
   messages: Message[] = [];
   constructor() {
-    // for (let i = 0; i < 5; i++) {
-       this.messages.push(new Message(``, `Hello! How can I help you?`, false));
-    // }
+    this.messages.push(new Message(``, `Hello! How can I help you?`, false));
   }
 
   getMessages(): Observable<Message[]> {

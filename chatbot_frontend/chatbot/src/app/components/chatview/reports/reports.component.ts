44d2ad7e807f7991ea912d6d4dcaa20569b29ad5
Krystian Szabat <221662@edu.p.lodz.pl>
2018-05-27 21:38:21 +0200

    [MC-76] - Reports format

--- chatbot_frontend/chatbot/src/app/components/chatview/reports/reports.component.ts
+++ chatbot_frontend/chatbot/src/app/components/chatview/reports/reports.component.ts
@@ -31,12 +31,11 @@ export class ReportsComponent implements OnInit {
     }
 
     prepareQEI(data) {
-        console.log(Object.keys(data), Object.keys(data).length);
         for (let i = 0; i < Object.keys(data).length - 1; i++) {
             const key = Object.keys(data)[i];
             const nextKey = Object.keys(data)[i + 1];
 
-            if (parseInt(data[key], 10) !== 0 && parseInt(data[nextKey], 10) !== 0) {
+            if (data[key] && data[nextKey] && parseInt(data[key], 10) !== 0 && parseInt(data[nextKey], 10) !== 0) {
                 this.productsQEI.push(new QEIItem(parseInt(data[key], 10), parseInt(data[nextKey], 10)));
             }
         }

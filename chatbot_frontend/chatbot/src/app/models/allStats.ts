334dfbf897afecb89d1f136c8edef3b2aa7cbc85
Krystian Szabat <221662@edu.p.lodz.pl>
2018-05-27 21:28:24 +0200

    [MC-76] - Reports

---
+++ chatbot_frontend/chatbot/src/app/models/allStats.ts
@@ -0,0 +1,13 @@
+export class AllStats {
+    avgAmountOfMisunderstoodQuestions: number;
+    avgAmountOfQuestions: number;
+    avgEffectivenessScore: number;
+    avgUsabilityScore: number;
+
+    constructor() {
+        this.avgAmountOfMisunderstoodQuestions = 0;
+        this.avgAmountOfQuestions = 0;
+        this.avgEffectivenessScore = 0;
+        this.avgUsabilityScore = 0;
+    }
+}

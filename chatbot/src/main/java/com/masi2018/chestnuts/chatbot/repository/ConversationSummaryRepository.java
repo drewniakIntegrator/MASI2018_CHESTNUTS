f06c9325fc5376b3e2b006eeb06320a358b43d44
nadarzynskiDeveloper <221634@edu.p.lodz.pl>
2018-05-28 17:12:27 +0200

    [MC-94] report now contains data only about certain username and certain address

--- chatbot/src/main/java/com/masi2018/chestnuts/chatbot/repository/ConversationSummaryRepository.java
+++ chatbot/src/main/java/com/masi2018/chestnuts/chatbot/repository/ConversationSummaryRepository.java
@@ -4,9 +4,13 @@ import com.masi2018.chestnuts.chatbot.model.ConversationSummary;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ConversationSummaryRepository extends MongoRepository<ConversationSummary, String> {
 
     ConversationSummary findByConversationId(String conversationId);
+
+    List<ConversationSummary> findAllByUsernameAndUserAddress(String username, String userAddress);
 
 }
